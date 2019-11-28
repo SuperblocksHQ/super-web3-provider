6f52b7e98ed08509ecbb5b27f8325c281ef4a941
Javier Tarazaga <javier.tarazaga@valtech.se>
2019-11-28 23:31:29 +0100

    A bit of refactoring

--- src/super.provider.ts
+++ src/super.provider.ts
@@ -48,7 +48,6 @@ export default class SuperblocksProvider {
     private readonly CI_JOB_ID: string = process.env.CI_JOB_ID;
 
     private options: IProviderOptions;
-    private pending: any = {};
 
     constructor(options: IProviderOptions) {
         this.options = options;
@@ -56,96 +55,73 @@ export default class SuperblocksProvider {
         this.init();
     }
 
-    public handleMessage(msg: IMessage) {
-        if (this.pending[msg.id]) {
-            const cb = this.pending[msg.id];
-            delete this.pending[msg.id];
-
-            if (msg.payload.err) {
-                this.log(`error occurred: ${msg.payload.err}`);
-            }
-            cb(msg.payload.err, msg.payload.res);
-        }
-    }
-
-    public async sendMessage(payload: IRPCPayload, networkId: string, callback: any) {
-        if (payload.method === 'eth_accounts') {
-            callback(null, {
-                jsonrpc: payload.jsonrpc,
-                id: payload.id,
-                result: this.accounts
-            });
-        } else if (payload.method === 'eth_sendTransaction' || payload.method === 'eth_sign') {
-            const transaction = await superblocksClient.sendEthTransaction({
-                buildConfigId: this.BUILD_CONFIG_ID,
-                ciJobId: this.CI_JOB_ID,
-                projectId: this.PROJECT_ID,
-                networkId,
-                from: this.options.from,
-                rpcPayload: payload
-            });
-
-            // We can only subscribe to the transaction on this precise moment, as otherwise we won't have the proper JobId mapped
-            subscribeToChannel(`web3-hub-${transaction.jobId}`, ['update_transaction'], (event) => {
-                if (event.eventName === 'update_transaction') {
-                    const txUpdated: ITransactionModel = event.message;
-
-                    // Unsubscribe immediately after receiving the receipt txHash
-                    unsubscribeFromChannel(`web3-hub-${transaction.jobId}`);
-
-                    // TODO - Proper error handling here
-                    callback(null, {
-                        jsonrpc: payload.jsonrpc,
-                        id: payload.id,
-                        result: txUpdated.transactionHash
-                    });
-                }
-            });
-         } else {
-            // Methods which are not to be intercepted or do not need any account information could be
-            // offloaded to Infura, Etherscan, custom Ethereum node or some other public node
-            try {
-                const response = await fetch(this.options.endpoint, {
-                    body: JSON.stringify(payload),
-                    headers: {
-                        'content-type': 'application/json',
-                    },
-                    method: 'POST'
+    public async sendMessage(payload: IRPCPayload, networkId: string): Promise<any> {
+        return new Promise(async (resolve, rejects) => {
+            if (payload.method === 'eth_accounts') {
+                resolve({
+                    jsonrpc: payload.jsonrpc,
+                    id: payload.id,
+                    result: [this.options.from]
+                });
+            } else if (payload.method === 'eth_sendTransaction' || payload.method === 'eth_sign') {
+                const transaction = await superblocksClient.sendEthTransaction({
+                    buildConfigId: this.BUILD_CONFIG_ID,
+                    ciJobId: this.CI_JOB_ID,
+                    projectId: this.PROJECT_ID,
+                    networkId,
+                    from: this.options.from,
+                    rpcPayload: payload
                 });
 
-                const data = await response.json();
-                callback(null, data);
-            } catch (error) {
-                callback(error, null);
+                // We can only subscribe to the transaction on this precise moment, as otherwise we won't have the proper JobId mapped
+                subscribeToChannel(`web3-hub-${transaction.jobId}`, ['update_transaction'], (event) => {
+                    if (event.eventName === 'update_transaction') {
+                        const txUpdated: ITransactionModel = event.message;
+
+                        // TODO - Is his actually the right thing to do?
+                        // Unsubscribe immediately after receiving the receipt txHash
+                        unsubscribeFromChannel(`web3-hub-${transaction.jobId}`);
+
+                        // TODO - Proper error handling here
+                        resolve({
+                            jsonrpc: payload.jsonrpc,
+                            id: payload.id,
+                            result: txUpdated.transactionHash
+                        });
+                    }
+                });
+             } else {
+                // Methods which are not to be intercepted or do not need any account information could be
+                // offloaded to Infura, Etherscan, custom Ethereum node or some other public node
+                try {
+                    const response = await fetch(this.options.endpoint, {
+                        body: JSON.stringify(payload),
+                        headers: {
+                            'content-type': 'application/json',
+                        },
+                        method: 'POST'
+                    });
+
+                    const data = await response.json();
+                    resolve(data);
+                } catch (error) {
+                    rejects(error);
+                }
             }
-        }
+        });
     }
 
-    public prepareRequest(_async: any) {
-        throw new Error('Not implemented.');
+    public send(payload: IRPCPayload): Promise<any> {
+        return this.sendMessage(payload, this.options.networkId);
     }
 
-    public send(payload: IRPCPayload, callback: any) {
-        this.sendAsync(payload, callback);
-    }
-
-    public sendAsync(payload: any, callback: any) {
-        this.sendMessage(payload, this.options.networkId, callback);
-    }
-
-    public getAddress(index: number) {
-        return this.accounts[index];
-    }
-
-    get accounts() {
-        return [this.options.from];
+    public sendAsync(payload: IRPCPayload, callback: (error: any, result: any) => void): void {
+        this.sendMessage(payload, this.options.networkId)
+            .then((result) => callback(null, result))
+            .catch((error) => callback(error, null));
     }
 
     private init() {
         connectToPusher();
-    }
-
-    private log(msg: any) {
-        console.log('[SuperblocksProvider] ' + (msg !== null ? msg : '') );
     }
 }
