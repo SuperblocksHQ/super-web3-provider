1dffe7bde36af1e25fb50984f4c833983ad87579
Javier Tarazaga <javier.tarazaga@valtech.se>
2019-12-12 21:25:30 +0100

    Fixed some issues

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import 'reflect-metadata';
 import { container } from './ioc/ioc.config';
 import { TYPES } from './ioc/types';
 import { IManualSignProvider } from './ioc/interfaces';
-import * as ManualSignProvider from './providers/super.provider.facade';
+import { ManualSignProviderFacade as ManualSignProvider } from './providers/super.provider.facade';
 
 // Composition root
 export const manualSignProvider = container.get<IManualSignProvider>(TYPES.ManualSigningProvider);
--- src/superblocks/utils.ts
+++ src/superblocks/utils.ts
@@ -14,8 +14,10 @@
 // You should have received a copy of the GNU General Public License
 // along with Superblocks.  If not, see <http://www.gnu.org/licenses/>.
 
+import { injectable } from 'inversify';
 import { ISuperblocksUtils } from '../ioc/interfaces';
 
+@injectable()
 export class SuperblocksUtils implements ISuperblocksUtils {
     getApiBaseUrl() {
         if (process.env.DEBUG) {
